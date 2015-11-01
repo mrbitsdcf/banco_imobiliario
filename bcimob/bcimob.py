63f24c96a7107f927567e0f97bfc094186a6b633
Diogo Fernandes <mrbits.dcf@gmail.com>
2015-11-01 10:43:40 -0200

    More fun with click and console functions

--- bcimob/bcimob.py
+++ bcimob/bcimob.py
@@ -14,12 +14,16 @@ click.clear()
 @click.group(context_settings=CONTEXT_SETTINGS)
 def cli():
     '''Gerencia dinheiro de jogadores no Banco Imobiliario'''
+    click.secho('Banco Imobiliario - Movimentação de Jogo', fg='green', bold=True)
+    click.secho('========================================\n\n', fg='green', bold=True)
     pass
 
 
 @cli.command(name='initialisedb', short_help="Inicia o banco de dados")
 def initialisedb_command():
+    click.secho('Criando banco de dados vazio', fg='blue')
     initialisedb()
+    click.secho('[OK]', fg='green', bold=True)
 
 
 @cli.command(name='add_players', short_help="Adiciona jogadores")
@@ -27,7 +31,7 @@ def initialisedb_command():
 def add_players_command(num_of_players):
     '''num_of_players: Numero de jogadores'''
     for p in range(int(num_of_players)):
-        player_name = 'Player {}'.format(p + 1)
+        player_name = 'Jogador {}'.format(p + 1)
         player = Player(player_name=player_name)
         DBSession.add(player)
 
@@ -41,12 +45,12 @@ def add_players_command(num_of_players):
 @click.argument('num_of_players')
 def start_game_command(num_of_players):
     '''num_of_players: Numero de jogadores'''
-    click.secho("Iniciando novo jogo com {} jogadores".format(num_of_players), fg='yellow')
+    click.secho("Iniciando novo jogo com {} jogadores\n".format(num_of_players), fg='yellow', bold=True)
     initialisedb()
     with click.progressbar(range(int(num_of_players))) as players:
         for p in players:
-            player_name = 'Player {}'.format(p + 1)
-            click.secho(' Inserindo jogador {0}'.format(player_name), fg='green')
+            player_name = 'Jogador {}'.format(p + 1)
+            click.secho(' Inserindo jogador {0}'.format(player_name), fg='blue')
             player = Player(player_name=player_name)
             DBSession.add(player)
 
@@ -55,18 +59,24 @@ def start_game_command(num_of_players):
 
     DBSession.flush()
     DBSession.commit()
+    click.secho('[OK]', fg='green', bold=True)
 
 
 @cli.command(name='list_balance', short_help="Lista dinheiro dos jogadores")
 @click.argument('players', default='ALL')
 def list_balance_command(players):
     '''players: Nome do jogador. Deixar sem valor para listar todos'''
+    click.secho('Listando balanco de jogadores\n', fg='yellow', bold=True)
     rs_players = Player.query.filter(Player.player_name != 'Banqueiro')
     if players != 'ALL':
         rs_players = rs_players.filter(Player.id == players).first()
 
+    click.secho('Jogador     Balance', fg='red', bold=True)
+    click.secho('=========================', fg='red')
     for player in rs_players:
-        print "Player: {0} - Balance: BI$ {1}".format(player.player_name, money.format_money(player.balance))
+        click.secho("{0} - BI${1:>10}".format(player.player_name, money.format_money(player.balance)))
+
+    print
 
 
 @cli.command(name='list_movement', short_help="Lista movimentacao dos jogadores")
@@ -82,7 +92,7 @@ def list_movement(players):
 
     for movement in movements:
         movement_type = Movement.VERBOSE_TYPES[Movement.REV_TYPES[movement.move_type]]
-        print "Player: {0} - Tipo: {1} - Valor: BI$ {2}".format(movement.player.player_name, movement_type, movement.amount)
+        print "Jogador: {0} - Tipo: {1} - Valor: BI$ {2}".format(movement.player.player_name, movement_type, movement.amount)
 
 
 @cli.command(name='movement', short_help='Movimenta dinheiro')
@@ -91,7 +101,8 @@ def list_movement(players):
 @click.argument('player_in', default=None)
 @click.argument('amount')
 def movement_command(mov_type, player_out=None, player_in=None, amount=0.0):
-    """mov_type: Tipo de movimento. Recebe IN para entrada, OUT para saida e PAY para pagamento
+    """\b
+    mov_type: Tipo de movimento. Recebe 'in' para entrada, 'out' para saida e 'pay' para pagamento
 
     player_out: Numero do jogador que pagara
 
@@ -103,24 +114,29 @@ def movement_command(mov_type, player_out=None, player_in=None, amount=0.0):
     Exemplos de uso:
 
     \b
-        $ bcimob movement IN 1 1 2000       # Jogador 1 receberá do banco BI$ 2.000,00
+        $ bcimob movement in 1 1 2000       # Jogador 1 receberá do banco BI$ 2.000,00
 
     \b
-        $ bcimob movement OUT 2 2 2000      # Jogador 2 pagará ao banco BI$ 2.000,00
+        $ bcimob movement out 2 2 2000      # Jogador 2 pagará ao banco BI$ 2.000,00
 
     \b
-        $ bcimob movement PAY 1 2 2000      # Jogador 1 pagará BI$ 2.000,00 ao jogador 2
+        $ bcimob movement pay 1 2 2000      # Jogador 1 pagará BI$ 2.000,00 ao jogador 2
 
     """
-    if mov_type == 'IN':
-        print "Player {0} recebendo {1}".format(player_in, amount)
+
+    click.secho('Movimentacao de dinheiro\n', fg='yellow', bold=True)
+    fmt_amount = money.format_money(money.Decimal(amount))
+    if mov_type == 'in':
+        print "Jogador {0} recebendo BI$ {1}".format(player_in, fmt_amount)
         add_money(player_in, float(amount))
-    elif mov_type == 'OUT':
-        print "Player {0} pagando {1}".format(player_out, amount)
+    elif mov_type == 'out':
+        print "Jogador {0} pagando {1}".format(player_out, fmt_amount)
         subtract_money(player_out, float(amount))
-    elif mov_type == 'PAY':
-        print "Player {0} pagando {1} para Player {2}".format(player_out, amount, player_in)
+    elif mov_type == 'pay':
+        print "Jogador {0} pagando {1} para Jogador {2}".format(player_out, fmt_amount, player_in)
         pay_money(player_out, player_in, float(amount))
+
+    click.secho('[OK]', fg='green', bold=True)
 
 if __name__ == '__main__':
     cli()
